75280f99241d2a042f3c7005ecd1a0d7a22c4c53
andrei lorin <andreilorin@gmail.com>
2017-04-02 22:04:57 +0100

    easy functions

--- HelloWorld/Main.cpp
+++ HelloWorld/Main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int add(int x, int y) {
+	return x + y;
+}
+
+void greet(string name)
+{
+	string greet = "Hello ";
+
+	cout << greet + name << std::endl;
+	cin.get();
+
+}
+
 
 int main()
 {
+	int c = add(10, 20);
+
+	greet("Andrei");
+
+	std::cout << c << std::endl;
 	std::cout << "Hello World !" << std::endl;
 	std::cin.get();
 }
+
